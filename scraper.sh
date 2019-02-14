447c7a2ff4c76825617c8a6b6b46f4f1c02d1ce5
Neev Parikh <neev_parikh@brown.edu>
2019-02-14 16:29:38 -0500

    Updating files

--- scraper.sh
+++ scraper.sh
@@ -1,3 +1,4 @@
+dir="/home/neev/GDrive/College/Brown University/Classes/Spring 2019/CSCI 143/projects/"
 wget -i urls.txt -P tmp/ -q -a logs.txt
 for i in tmp/*.zip; do unzip "$i" -d "${i%%.zip}"; done
 rm tmp/*.zip >> logs.txt
@@ -5,5 +6,6 @@ mv tmp/* ./
 git add --all >> logs.txt
 git commit -m "Updating files" >> logs.txt
 git push >> logs.txt
-rm -r tmp/ >> logs.txt
+rm -r tmp/ >> logs.txti
+cp ./pro* $dir
 mkdir tmp/
