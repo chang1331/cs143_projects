4b7e1cb12ac41c5d783f622e31a3f82dc076022d
Neev Parikh <neev_parikh@brown.edu>
2019-02-14 16:10:42 -0500

    Updated script for pulling projects

--- scraper.sh
+++ scraper.sh
@@ -1,10 +1,9 @@
 wget -i urls.txt -P tmp/ -q -a logs.txt
 for i in tmp/*.zip; do unzip "$i" -d "${i%%.zip}"; done
 rm tmp/*.zip >> logs.txt
-cd tmp/
+mv tmp/* ./
 git add --all >> logs.txt
 git commit -m "Updating files" >> logs.txt
 git push >> logs.txt
-cd ..
 rm -r tmp/ >> logs.txt
 mkdir tmp/
