1a6389629f1dafc79a5a6d62b2e5dd15de141822
Neev Parikh <neev_parikh@brown.edu>
2019-02-19 18:15:54 -0500

    Updating files

--- scraper.sh
+++ scraper.sh
@@ -2,6 +2,7 @@ wget -i urls.txt -P tmp/ -q -a logs.txt
 for i in tmp/*.zip; do unzip "$i" -d "${i%%.zip}"; done
 rm tmp/*.zip*
 rm projects/proj*
+mkdir projects/
 mv tmp/* ./projects/
 git add --all 
 git commit -m "Updating files" 
